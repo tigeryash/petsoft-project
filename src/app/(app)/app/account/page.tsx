1abbbb5961064a18731eff35b355e62a34aebdc9
tigeryash <yashwanth.venkatesan@gmail.com>
2024-04-10 23:02:51 -0400

    edited imports in page.tsx

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,21 +1,20 @@
-import H1 from "@/components/H1";
 import ContentBlock from "@/components/content-block";
+import H1 from "@/components/H1";
 import SignOutBtn from "@/components/sign-out-btn";
 import { checkAuth } from "@/lib/server-utils";
 
-const AccountPage = async () => {
+export default async function Page() {
   const session = await checkAuth();
 
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
-      <ContentBlock className="h-[500px] flex flex-col justify-center items-center">
-        <p> logged in as {session.user.email}</p>
+      <ContentBlock className="h-[500px] flex flex-col gap-3 justify-center items-center">
+        <p>Logged in as {session.user.email}</p>
+
         <SignOutBtn />
       </ContentBlock>
     </main>
   );
-};
-
-export default AccountPage;
+}
--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -6,22 +6,24 @@ import PetList from "@/components/pet-list";
 import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
-export const Dashboard = () => {
+export default async function Page() {
   return (
     <main>
-      <div className="flex justify-between items-center text-white py-8">
+      <div className="flex items-center justify-between text-white py-8">
         <Branding />
 
         <Stats />
       </div>
 
       <div className="grid md:grid-cols-3 md:grid-rows-[45px_1fr] grid-rows-[45px_300px_500px] gap-4 md:h-[600px]">
-        <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1 ">
+        <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1">
           <SearchForm />
         </div>
-        <div className="md:row-start-2 md:row-span-full md:col-start-1 md:ol-span-1 relative">
+
+        <div className="relative md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
           <ContentBlock>
             <PetList />
+
             <div className="absolute bottom-4 right-4">
               <PetButton actionType="add" />
             </div>
@@ -36,6 +38,4 @@ export const Dashboard = () => {
       </div>
     </main>
   );
-};
-
-export default Dashboard;
+}
